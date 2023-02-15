c25a4e285988595ea9cbb1b4f82f3fd232508e78
张占龙 <zhangzl@hzwangda.com>
2023-02-15 22:35:56 +0800

    资产管理优化

--- src/api/assetFloot.js
+++ src/api/assetFloot.js
@@ -1,53 +1,53 @@
 import request from '@/utils/request'
 export function fetchList(params) {
   return request({
-    url:'/assetFloot/list',
-    method:'get',
-    params:params
+    url: '/assetFloot/list',
+    method: 'get',
+    params: params
   })
 }
 export function createBrand(data) {
   return request({
-    url:'/assetFloot/create',
-    method:'post',
-    data:data
+    url: '/assetFloot/create',
+    method: 'post',
+    data: data
   })
 }
 export function updateShowStatus(data) {
   return request({
-    url:'/assetFloot/update/showStatus',
-    method:'post',
-    data:data
+    url: '/assetFloot/update/showStatus',
+    method: 'post',
+    data: data
   })
 }
 
 export function updateFactoryStatus(data) {
   return request({
-    url:'/assetFloot/update/factoryStatus',
-    method:'post',
-    data:data
+    url: '/assetFloot/update/factoryStatus',
+    method: 'post',
+    data: data
   })
 }
 
 export function deleteBrand(id) {
   return request({
-    url:'/assetFloot/delete/'+id,
-    method:'get',
+    url: '/assetFloot/delete/' + id,
+    method: 'get'
   })
 }
 
 export function getBrand(id) {
   return request({
-    url:'/assetFloot/'+id,
-    method:'get',
+    url: '/assetFloot/' + id,
+    method: 'get'
   })
 }
 
-export function updateBrand(id,data) {
+export function updateBrand(id, data) {
   return request({
-    url:'/assetFloot/update/'+id,
-    method:'post',
-    data:data
+    url: '/assetFloot/update/' + id,
+    method: 'post',
+    data: data
   })
 }
 
--- src/views/asset/assetFloor/index.vue
+++ src/views/asset/assetFloor/index.vue
@@ -1,67 +1,71 @@
-<template> 
+<template>
   <div class="app-container">
     <el-card class="filter-container" shadow="never">
-        <div>
-          <i class="el-icon-search"></i>
-          <span>筛选搜索</span>
-          <el-button
-            style="float: right"
-            @click="searchBrandList()"
-            type="primary"
-            size="small">
-            查询结果
-          </el-button>
-        </div>
-        <div style="margin-top: 15px">
-          <el-form :inline="true" :model="listQuery" size="small" label-width="140px">
-            <el-form-item label="输入搜索：">
-              <el-input style="width: 203px" v-model="listQuery.keyword" placeholder="品牌名称/关键字"></el-input>
-            </el-form-item>
-          </el-form>
-        </div>
+      <div>
+        <i class="el-icon-search" />
+        <span>筛选搜索</span>
+        <el-button
+          style="float: right"
+          type="primary"
+          size="small"
+          @click="searchBrandList()"
+        >
+          查询结果
+        </el-button>
+      </div>
+      <div style="margin-top: 15px">
+        <el-form :inline="true" :model="listQuery" size="small" label-width="140px">
+          <el-form-item label="输入搜索：">
+            <el-input v-model="listQuery.keyword" style="width: 203px" placeholder="资产名称/关键字" />
+          </el-form-item>
+        </el-form>
+      </div>
     </el-card>
     <el-card class="operate-container" shadow="never">
-      <i class="el-icon-tickets"></i>
+      <i class="el-icon-tickets" />
       <span>数据列表</span>
       <el-button
         class="btn-add"
+        size="mini"
         @click="addBrand()"
-        size="mini">
+      >
         添加
       </el-button>
     </el-card>
     <div class="table-container">
-      <el-table ref="brandTable"
-                :data="list"
-                style="width: 100%"
-                @selection-change="handleSelectionChange"
-                v-loading="listLoading"
-                border>
-        <el-table-column type="selection" width="60" align="center"></el-table-column>
-        <el-table-column type="index" label="编号" width="100" align="center"></el-table-column>
+      <el-table
+        ref="brandTable"
+        v-loading="listLoading"
+        :data="list"
+        style="width: 100%"
+        border
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="60" align="center" />
+        <el-table-column type="index" label="编号" width="100" align="center" />
         <el-table-column label="资产名称" align="center">
-          <template slot-scope="scope">{{scope.row.name}}</template>
+          <template slot-scope="scope">{{ scope.row.name }}</template>
         </el-table-column>
         <el-table-column label="资产地址" width="100" align="center">
-          <template slot-scope="scope">{{scope.row.address}}</template>
+          <template slot-scope="scope">{{ scope.row.address }}</template>
         </el-table-column>
         <el-table-column label="所属地区" width="100" align="center">
-          <template slot-scope="scope">{{scope.row.region}}</template>
+          <template slot-scope="scope">{{ scope.row.region }}</template>
         </el-table-column>
         <el-table-column label="联系人" width="100" align="center">
-          <template slot-scope="scope">{{scope.row.lxr}}</template>
+          <template slot-scope="scope">{{ scope.row.lxr }}</template>
         </el-table-column>
         <el-table-column label="联系电话" width="100" align="center">
-          <template slot-scope="scope">{{scope.row.lxdh}}</template>
+          <template slot-scope="scope">{{ scope.row.lxdh }}</template>
         </el-table-column>
         <el-table-column label="展示状态" width="100" align="center">
           <template slot-scope="scope">
             <el-switch
+              v-model="scope.row.zszt"
+              :active-value="'1'"
+              :inactive-value="'0'"
               @change="handleFactoryStatusChange(scope.$index, scope.row)"
-              :active-value="1"
-              :inactive-value="0"
-              v-model="scope.row.zszt">
-            </el-switch>
+            />
           </template>
         </el-table-column>
         <el-table-column label="相关" width="220" align="center">
@@ -70,13 +74,15 @@
             <el-button
               size="mini"
               type="text"
-              @click="getProductList(scope.$index, scope.row)">100
+              @click="getProductList(scope.$index, scope.row)"
+            >100
             </el-button>
             <span>评价：</span>
             <el-button
               size="mini"
               type="text"
-              @click="getProductCommentList(scope.$index, scope.row)">1000
+              @click="getProductCommentList(scope.$index, scope.row)"
+            >1000
             </el-button>
           </template>
         </el-table-column>
@@ -84,220 +90,202 @@
           <template slot-scope="scope">
             <el-button
               size="mini"
-              @click="handleUpdate(scope.$index, scope.row)">编辑
+              @click="handleUpdate(scope.$index, scope.row)"
+            >编辑
             </el-button>
             <el-button
               size="mini"
               type="danger"
-              @click="handleDelete(scope.$index, scope.row)">删除
+              @click="handleDelete(scope.$index, scope.row)"
+            >删除
             </el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
-    <div class="batch-operate-container">
-      <el-select
-        size="small"
-        v-model="operateType" placeholder="批量操作">
-        <el-option
-          v-for="item in operates"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-      <el-button
-        style="margin-left: 20px"
-        class="search-button"
-        @click="handleBatchOperate()"
-        type="primary"
-        size="small">
-        确定
-      </el-button>
-    </div>
+
     <div class="pagination-container">
       <el-pagination
         background
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
         layout="total, sizes,prev, pager, next,jumper"
         :page-size="listQuery.pageSize"
         :page-sizes="[5,10,15]"
         :current-page.sync="listQuery.pageNum"
-        :total="total">
-      </el-pagination>
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
 <script>
-  import {fetchList, updateShowStatus, updateFactoryStatus, deleteBrand} from '@/api/assetFloot'
+import { fetchList, updateShowStatus, updateFactoryStatus, deleteBrand } from '@/api/assetFloot'
 
-  export default {
-    name: 'brandList',
-    data() {
-      return {
-        operates: [
-          {
-            label: "显示品牌",
-            value: "showBrand"
-          },
-          {
-            label: "隐藏品牌",
-            value: "hideBrand"
-          }
-        ],
-        operateType: null,
-        listQuery: {
-          keyword: null,
-          pageNum: 1,
-          pageSize: 10
+export default {
+  name: 'BrandList',
+  data() {
+    return {
+      operates: [
+        {
+          label: '显示品牌',
+          value: 'showBrand'
         },
-        list: null,
-        total: null,
-        listLoading: true,
-        multipleSelection: []
-      }
-    },
-    created() {
-      this.getList();
-    },
-    methods: {
-      getList() {
-        this.listLoading = true;
-        fetchList(this.listQuery).then(response => {
-          this.listLoading = false;
-          this.list = response.data.list;
-          this.total = response.data.total;
-          this.totalPage = response.data.totalPage;
-          this.pageSize = response.data.pageSize;
-        });
-      },
-      handleSelectionChange(val) {
-        this.multipleSelection = val;
-      },
-      handleUpdate(index, row) {
-        this.$router.push({path: '/asset/updateBrand', query: {id: row.id}})
-      },
-      handleDelete(index, row) {
-        this.$confirm('是否要删除该品牌', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          deleteBrand(row.id).then(response => {
-            this.$message({
-              message: '删除成功',
-              type: 'success',
-              duration: 1000
-            });
-            this.getList();
-          });
-        });
-      },
-      getProductList(index, row) {
-        console.log(index, row);
-      },
-      getProductCommentList(index, row) {
-        console.log(index, row);
-      },
-      handleFactoryStatusChange(index, row) {
-        var data = new URLSearchParams();
-        data.append("ids", row.id);
-        data.append("factoryStatus", row.factoryStatus);
-        updateFactoryStatus(data).then(response => {
-          this.$message({
-            message: '修改成功',
-            type: 'success',
-            duration: 1000
-          });
-        }).catch(error => {
-          if (row.factoryStatus === 0) {
-            row.factoryStatus = 1;
-          } else {
-            row.factoryStatus = 0;
-          }
-        });
-      },
-      handleShowStatusChange(index, row) {
-        let data = new URLSearchParams();
-        ;
-        data.append("ids", row.id);
-        data.append("showStatus", row.showStatus);
-        updateShowStatus(data).then(response => {
-          this.$message({
-            message: '修改成功',
-            type: 'success',
-            duration: 1000
-          });
-        }).catch(error => {
-          if (row.showStatus === 0) {
-            row.showStatus = 1;
-          } else {
-            row.showStatus = 0;
-          }
-        });
-      },
-      handleSizeChange(val) {
-        this.listQuery.pageNum = 1;
-        this.listQuery.pageSize = val;
-        this.getList();
-      },
-      handleCurrentChange(val) {
-        this.listQuery.pageNum = val;
-        this.getList();
-      },
-      searchBrandList() {
-        this.listQuery.pageNum = 1;
-        this.getList();
-      },
-      handleBatchOperate() {
-        console.log(this.multipleSelection);
-        if (this.multipleSelection < 1) {
-          this.$message({
-            message: '请选择一条记录',
-            type: 'warning',
-            duration: 1000
-          });
-          return;
+        {
+          label: '隐藏品牌',
+          value: 'hideBrand'
         }
-        let showStatus = 0;
-        if (this.operateType === 'showBrand') {
-          showStatus = 1;
-        } else if (this.operateType === 'hideBrand') {
-          showStatus = 0;
+      ],
+      operateType: null,
+      listQuery: {
+        keyword: null,
+        pageNum: 1,
+        pageSize: 10
+      },
+      list: null,
+      total: null,
+      listLoading: true,
+      multipleSelection: []
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.listLoading = false
+        this.list = response.data.list
+        this.total = response.data.total
+        this.totalPage = response.data.totalPage
+        this.pageSize = response.data.pageSize
+      })
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    handleUpdate(index, row) {
+      this.$router.push({ path: '/asset/updateBrand', query: { id: row.id }})
+    },
+    handleDelete(index, row) {
+      this.$confirm('是否要删除该品牌', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        deleteBrand(row.id).then(response => {
+          this.$message({
+            message: '删除成功',
+            type: 'success',
+            duration: 1000
+          })
+          this.getList()
+        })
+      })
+    },
+    getProductList(index, row) {
+      console.log(index, row)
+    },
+    getProductCommentList(index, row) {
+      console.log(index, row)
+    },
+    handleFactoryStatusChange(index, row) {
+      var data = new URLSearchParams()
+      data.append('ids', row.id)
+      data.append('zszt', row.zszt)
+      updateFactoryStatus(data).then(response => {
+        this.$message({
+          message: '修改成功',
+          type: 'success',
+          duration: 1000
+        })
+      }).catch(error => {
+        console.log(error)
+        if (row.zszt === 0) {
+          row.zszt = 1
         } else {
-          this.$message({
-            message: '请选择批量操作类型',
-            type: 'warning',
-            duration: 1000
-          });
-          return;
+          row.zszt = 0
         }
-        let ids = [];
-        for (let i = 0; i < this.multipleSelection.length; i++) {
-          ids.push(this.multipleSelection[i].id);
+      })
+    },
+    handleShowStatusChange(index, row) {
+      const data = new URLSearchParams()
+
+      data.append('ids', row.id)
+      data.append('showStatus', row.showStatus)
+      updateShowStatus(data).then(response => {
+        this.$message({
+          message: '修改成功',
+          type: 'success',
+          duration: 1000
+        })
+      }).catch(error => {
+        if (row.showStatus === 0) {
+          row.showStatus = 1
+        } else {
+          row.showStatus = 0
         }
-        let data = new URLSearchParams();
-        data.append("ids", ids);
-        data.append("showStatus", showStatus);
-        updateShowStatus(data).then(response => {
-          this.getList();
-          this.$message({
-            message: '修改成功',
-            type: 'success',
-            duration: 1000
-          });
-        });
-      },
-      addBrand() {
-        this.$router.push({path: '/asset/addBrand'})
+      })
+    },
+    handleSizeChange(val) {
+      this.listQuery.pageNum = 1
+      this.listQuery.pageSize = val
+      this.getList()
+    },
+    handleCurrentChange(val) {
+      this.listQuery.pageNum = val
+      this.getList()
+    },
+    searchBrandList() {
+      this.listQuery.pageNum = 1
+      this.getList()
+    },
+    handleBatchOperate() {
+      console.log(this.multipleSelection)
+      if (this.multipleSelection < 1) {
+        this.$message({
+          message: '请选择一条记录',
+          type: 'warning',
+          duration: 1000
+        })
+        return
       }
+      let showStatus = 0
+      if (this.operateType === 'showBrand') {
+        showStatus = 1
+      } else if (this.operateType === 'hideBrand') {
+        showStatus = 0
+      } else {
+        this.$message({
+          message: '请选择批量操作类型',
+          type: 'warning',
+          duration: 1000
+        })
+        return
+      }
+      const ids = []
+      for (let i = 0; i < this.multipleSelection.length; i++) {
+        ids.push(this.multipleSelection[i].id)
+      }
+      const data = new URLSearchParams()
+      data.append('ids', ids)
+      data.append('showStatus', showStatus)
+      updateShowStatus(data).then(response => {
+        this.getList()
+        this.$message({
+          message: '修改成功',
+          type: 'success',
+          duration: 1000
+        })
+      })
+    },
+    addBrand() {
+      this.$router.push({ path: '/asset/addBrand' })
     }
   }
+}
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
 
-
 </style>
-
 
